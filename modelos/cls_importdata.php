8d3ef10e06e94ea39c3cd0bcb372ac1fe80b1d03
unknown <desarrolladorsoftware@concretol.com>
2022-02-10 07:48:50 -0500

    Se agrego la funcion de notas de inventario

--- modelos/cls_importdata.php
+++ modelos/cls_importdata.php
@@ -1,20 +1,21 @@
 <?php
-class cls_importdata extends conexionPDO {
+class cls_importdata extends conexionPDO
+{
 
     public $con;
 
     // Iniciar Conexion
-    public function __construct() {
+    public function __construct()
+    {
         $this->PDO = new conexionPDO();
         $this->con = $this->PDO->connect();
     }
     function insert_centro_costo(array $array_datos)
     {
-        if(is_array($array_datos))
-        {
+        if (is_array($array_datos)) {
             foreach ($array_datos as $row) {
-                
-                $sql=" INSERT INTO `centrocostos`( `codigo`, `nombre`, `codigocompleto`) VALUES (:codigo,:nombre,:codigocompleto)";
+
+                $sql = " INSERT INTO `centrocostos`( `codigo`, `nombre`, `codigocompleto`) VALUES (:codigo,:nombre,:codigocompleto)";
                 $stmt = $this->con->prepare($sql); // Preparar la conexion
                 $stmt->bindParam(':codigo', $row['codigo'], PDO::PARAM_STR);
                 $stmt->bindParam(':nombre', $row['nombre'], PDO::PARAM_STR);
@@ -22,26 +23,22 @@ class cls_importdata extends conexionPDO {
 
                 if ($stmt->execute()) { // Ejecutar
                     $result = " Exitosso";
-                }else{
+                } else {
                     $result = "Error";
-
                 }
-
             }
             return $result;
-        }else{
+        } else {
             return false;
         }
-        
     }
 
     function insert_alance_comprobacion(array $array_datos)
     {
-        if(is_array($array_datos))
-        {
+        if (is_array($array_datos)) {
             foreach ($array_datos as $row) {
-                
-                $sql=" INSERT INTO `balance_corporativo`( `puc`, `terceros`, `cco`, `scc`, `nombre`, `saldo_anterior`, `movimiento_debito`, `movimiento_credito`, `nuevo_saldo`, `fecha_corte`) VALUES  (:puc, :terceros, :cco, :scc, :nombre, :saldo_anterior, :movimiento_debito, :movimiento_credito, :nuevo_saldo , :fecha_corte)";
+
+                $sql = " INSERT INTO `balance_corporativo`( `puc`, `terceros`, `cco`, `scc`, `nombre`, `saldo_anterior`, `movimiento_debito`, `movimiento_credito`, `nuevo_saldo`, `fecha_corte`) VALUES  (:puc, :terceros, :cco, :scc, :nombre, :saldo_anterior, :movimiento_debito, :movimiento_credito, :nuevo_saldo , :fecha_corte)";
                 $stmt = $this->con->prepare($sql); // Preparar la conexion
                 $stmt->bindParam(':puc', $row['puc'], PDO::PARAM_STR);
                 $stmt->bindParam(':terceros', $row['tercero'], PDO::PARAM_STR);
@@ -55,26 +52,22 @@ class cls_importdata extends conexionPDO {
                 $stmt->bindParam(':fecha_corte', $row['fecha_corte'], PDO::PARAM_STR);
                 if ($stmt->execute()) { // Ejecutar
                     $result = " Exitosso";
-                }else{
+                } else {
                     $result = "Error";
-
                 }
-
             }
             return $result;
-        }else{
+        } else {
             return false;
         }
-        
     }
 
     function insert_movimiento_diario(array $array_datos)
     {
-        if(is_array($array_datos))
-        {
+        if (is_array($array_datos)) {
             foreach ($array_datos as $row) {
-                
-                $sql="INSERT INTO `movimiento_diario`(`tipo`, `numero`, `numero_cheque`, `Num_extension`, `anio`, `mes`, `dia`, `cuenta`, `nit`, `terceros`, `suc_pto`, `drocela`, `c_costo`, `sc_costo`, `detalles`, `debito`, `credito`, `elaborado`) VALUES(:tipo, :numero, :numero_cheque, :num_extension, :anio, :mes, :dia, :cuenta, :nit, :terceros, :suc_pto, :drocela, :c_costo, :sc_costo, :detalles, :debito, :credito, :elaborado)";
+
+                $sql = "INSERT INTO `movimiento_diario`(`tipo`, `numero`, `numero_cheque`, `Num_extension`, `anio`, `mes`, `dia`, `cuenta`, `nit`, `terceros`, `suc_pto`, `drocela`, `c_costo`, `sc_costo`, `detalles`, `debito`, `credito`, `elaborado`) VALUES(:tipo, :numero, :numero_cheque, :num_extension, :anio, :mes, :dia, :cuenta, :nit, :terceros, :suc_pto, :drocela, :c_costo, :sc_costo, :detalles, :debito, :credito, :elaborado)";
                 $stmt = $this->con->prepare($sql); // Preparar la conexion
                 $stmt->bindParam(':tipo', $row['tipo'], PDO::PARAM_STR);
                 $stmt->bindParam(':numero', $row['numero'], PDO::PARAM_INT);
@@ -96,19 +89,53 @@ class cls_importdata extends conexionPDO {
                 $stmt->bindParam(':elaborado', $row['elaborado'], PDO::PARAM_STR);
                 if ($stmt->execute()) { // Ejecutar
                     $result = " Exitosso";
-                }else{
+                } else {
                     $result = "Error";
-
                 }
-
             }
             return $result;
-        }else{
+        } else {
             return false;
         }
-        
     }
 
-
-
+    function insert_notas_inventario(array $array_datos)
+    {
+        if (is_array($array_datos)) {
+            foreach ($array_datos as $row) {
+                $sql = "INSERT INTO `notas_inventario`(`referencia`, `servicio`, `detalle`, `cantidad`, `precio`, `valor_unidad`, `valor_iva`, `total_mas_valor_iva`, `iva`, `base_iva`, `t_iva`, `ico`, `referencia1`, `referencia2`, `referencia3`, `referencia4`, `unidad`, `referencia_proveedor`, `tercero`, `descripcion_adicional`, `fecha_mes`, `planta`) VALUES (:referencia, :servicio, :detalle, :cantidad, :precio, :valor_unidad, :valor_iva, :total_mas_valor_iva, :iva, :base_iva, :t_iva, :ico, :referencia1, :referencia2, :referencia3, :referencia4, :unidad, :referencia_proveedor, :tercero, :descripcion_adicional, :fecha_mes, :planta)";
+                $stmt = $this->con->prepare($sql); // Preparar la conexion
+                $stmt->bindParam(':referencia', $row['referencia'], PDO::PARAM_INT);
+                $stmt->bindParam(':servicio', $row['servicio'], PDO::PARAM_STR);
+                $stmt->bindParam(':detalle', $row['detalle'], PDO::PARAM_STR);
+                $stmt->bindParam(':cantidad', $row['cantidad'], PDO::PARAM_STR);
+                $stmt->bindParam(':precio', $row['precio'], PDO::PARAM_STR);
+                $stmt->bindParam(':valor_unidad', $row['valor_unidad'], PDO::PARAM_STR);
+                $stmt->bindParam(':valor_iva', $row['valor_iva'], PDO::PARAM_STR);
+                $stmt->bindParam(':total_mas_valor_iva', $row['total_mas_valor_iva'], PDO::PARAM_STR);
+                $stmt->bindParam(':iva', $row['iva'], PDO::PARAM_STR);
+                $stmt->bindParam(':base_iva', $row['base_iva'], PDO::PARAM_STR);
+                $stmt->bindParam(':t_iva', $row['t_iva'], PDO::PARAM_STR);
+                $stmt->bindParam(':ico', $row['ico'], PDO::PARAM_STR);
+                $stmt->bindParam(':referencia1', $row['referencia1'], PDO::PARAM_STR);
+                $stmt->bindParam(':referencia2', $row['referencia2'], PDO::PARAM_STR);
+                $stmt->bindParam(':referencia3', $row['referencia3'], PDO::PARAM_STR);
+                $stmt->bindParam(':referencia4', $row['referencia4'], PDO::PARAM_STR);
+                $stmt->bindParam(':unidad', $row['unidad'], PDO::PARAM_STR);
+                $stmt->bindParam(':referencia_proveedor', $row['referencia_proveedor'], PDO::PARAM_STR);
+                $stmt->bindParam(':tercero', $row['tercero'], PDO::PARAM_STR);
+                $stmt->bindParam(':descripcion_adicional', $row['descripcion_adicional'], PDO::PARAM_STR);
+                $stmt->bindParam(':fecha_mes', $row['fecha_mes'], PDO::PARAM_STR);
+                $stmt->bindParam(':planta', $row['planta'], PDO::PARAM_STR);
+                if ($stmt->execute()) { // Ejecutar
+                    $result = " Exitosso";
+                } else {
+                    $result = "Error";
+                }
+            }
+            return $result;
+        } else {
+            return false;
+        }
+    }
 }
---
+++ modules/import_data/notas_inventario/ajax_notas_inventario.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+
+require '../../../librerias/autoload.php';
+require '../../../modelos/autoload.php';
+require '../../../vendor/autoload.php';
+
+
+
+$php_estado = false;
+$php_result = "saludo desde el servidor";
+
+$php_fechatime = "".date("Y-m-d H:i:s");
+$image = htmlspecialchars($_FILES['file_notas_inventario']['name']);
+$ruta = htmlspecialchars($_FILES['file_notas_inventario']['tmp_name']);
+
+$php_fileexten = strrchr($_FILES['file_notas_inventario']['name'],".");
+$php_serial = strtoupper(substr(hash('sha1', $_FILES['file_notas_inventario']['name'].$php_fechatime),0,40)).$php_fileexten;
+
+
+$carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/internal/load_data/'; 
+$php_tempfoto = ('/internal/load_data/'.$php_serial);
+$php_movefile = move_uploaded_file($ruta,$carpeta_destino.$php_serial);
+
+
+$inputFileName = $_SERVER['DOCUMENT_ROOT'].$php_tempfoto;
+
+$cls_importdata = new cls_importdata();
+
+
+// Clase para Escoger celdas Especificas
+class MyReadFilter implements \PhpOffice\PhpSpreadsheet\Reader\IReadFilter {
+
+    public function readCell($column, $row, $worksheetName = '') {
+        // Read title row and rows 20 - 30
+        if ($row > 1 ) { // comenzamos desde 1 para omitir el titulo de las comulnas ubicadas en la fila 1(excel)
+            return true;
+        }
+        return false;
+    }
+}
+
+//$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+
+/**  identifica el tipo de archivo $inputFileName  **/
+$inputFileType = \PhpOffice\PhpSpreadsheet\IOFactory::identify($inputFileName);
+/**  Crear un nuevo lector $reader para el tipo de archivo  **/
+$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+
+// inicializar la Clase leer excel
+$reader->setReadFilter( new MyReadFilter() );
+
+/**  Cargar $inputFileName al objeto $Spreadsheet **/
+$spreadsheet = $reader->load($inputFileName);
+
+// Recorremos las Celdas del archivo Tomando como referencia que el excel el numero de celdas comienza desde 0
+$array_reg = $spreadsheet->getActiveSheet()->toArray();
+if (is_array($array_reg)) {
+    foreach ($array_reg as $row) {
+
+        if(!is_null($row[0])){
+            $new_array['referencia'] = $row[0];
+            $new_array['servicio'] = $row[1];
+            $new_array['detalle'] = $row[2];
+            $new_array['cantidad'] = $row[3];
+            $new_array['precio'] = $row[4];
+            $new_array['valor_unidad'] = $row[5];
+            $new_array['valor_iva'] = $row[6];
+            $new_array['total_mas_valor_iva'] = $row[7];
+            $new_array['iva'] = $row[8];
+            $new_array['base_iva'] = $row[9];
+            $new_array['t_iva'] = $row[10];
+            $new_array['ico'] = $row[11];
+            $new_array['referencia1'] = $row[12];
+            $new_array['referencia2'] = $row[13];
+            $new_array['referencia3'] = $row[14];
+            $new_array['referencia4'] = $row[15];
+            $new_array['unidad'] = $row[16];
+            $new_array['referencia_proveedor'] = $row[17];
+            $new_array['tercero'] = $row[18];
+            $new_array['descripcion_adicional'] = $row[19];
+            $new_array['fecha_mes'] = $row[20];
+            $new_array['planta'] = $row[21];
+
+            $fecha = new DateTime($row[9]);
+        $fecha_d_m_y = $fecha->format('Y/m/d');
+           
+            $new_array['fecha_corte'] = $fecha_d_m_y;
+            
+       /** variable final para guardar en la base de datos $new_array */
+       $new_arrayf[] = $new_array;
+        }
+       
+       
+    }
+}
+
+if($php_result= $cls_importdata->insert_notas_inventario($new_arrayf)){
+    $php_estado = true;
+}
+
+
+
+$datos = array(
+    'estado' => $php_estado,
+    'result' => $php_result,
+    'dataload' => $new_arrayf
+);
+
+
+echo json_encode($datos, JSON_FORCE_OBJECT);
