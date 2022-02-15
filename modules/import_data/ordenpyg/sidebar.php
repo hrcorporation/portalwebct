ebc72a7ca7cff0611e7bf0e4c40c01262a5cfc0d
unknown <desarrolladorsoftware@concretol.com>
2022-02-15 17:09:41 -0500

    Se modifico la tabla orden pyg

--- modules/import_data/ordenpyg/sidebar.php
+++ modules/import_data/ordenpyg/sidebar.php
@@ -23,7 +23,7 @@
                                 </p>
                             </a>
                         </li>
-                        <li class="nav-header">balance Comprobacion</li>
+                        <li class="nav-header">Orden PyG</li>
 
                         <li class="nav-item has-treeview">
                             <a href="" class="nav-link active">
