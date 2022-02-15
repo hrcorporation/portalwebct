07cee619d0357cbe93ca225e9e8ab378143719c9
unknown <desarrolladorsoftware@concretol.com>
2022-02-15 10:46:32 -0500

    Se modifico la tabla presupuesto de produccion para darle formato a las fechas

--- modules/import_data/pptoprod/ajax_pptoprod.php
+++ modules/import_data/pptoprod/ajax_pptoprod.php
@@ -11,30 +11,32 @@ require '../../../vendor/autoload.php';
 $php_estado = false;
 $php_result = "saludo desde el servidor";
 
-$php_fechatime = "".date("Y-m-d H:i:s");
+$php_fechatime = "" . date("Y-m-d H:i:s");
 $image = htmlspecialchars($_FILES['file_pptoprod']['name']);
 $ruta = htmlspecialchars($_FILES['file_pptoprod']['tmp_name']);
 
-$php_fileexten = strrchr($_FILES['file_pptoprod']['name'],".");
-$php_serial = strtoupper(substr(hash('sha1', $_FILES['file_pptoprod']['name'].$php_fechatime),0,40)).$php_fileexten;
+$php_fileexten = strrchr($_FILES['file_pptoprod']['name'], ".");
+$php_serial = strtoupper(substr(hash('sha1', $_FILES['file_pptoprod']['name'] . $php_fechatime), 0, 40)) . $php_fileexten;
 
 
-$carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/internal/load_data/'; 
-$php_tempfoto = ('/internal/load_data/'.$php_serial);
-$php_movefile = move_uploaded_file($ruta,$carpeta_destino.$php_serial);
+$carpeta_destino = $_SERVER['DOCUMENT_ROOT'] . '/internal/load_data/';
+$php_tempfoto = ('/internal/load_data/' . $php_serial);
+$php_movefile = move_uploaded_file($ruta, $carpeta_destino . $php_serial);
 
 
-$inputFileName = $_SERVER['DOCUMENT_ROOT'].$php_tempfoto;
+$inputFileName = $_SERVER['DOCUMENT_ROOT'] . $php_tempfoto;
 
 $cls_importdata = new cls_importdata();
 
 
 // Clase para Escoger celdas Especificas
-class MyReadFilter implements \PhpOffice\PhpSpreadsheet\Reader\IReadFilter {
+class MyReadFilter implements \PhpOffice\PhpSpreadsheet\Reader\IReadFilter
+{
 
-    public function readCell($column, $row, $worksheetName = '') {
+    public function readCell($column, $row, $worksheetName = '')
+    {
         // Read title row and rows 20 - 30
-        if ($row > 1 ) { // comenzamos desde 1 para omitir el titulo de las comulnas ubicadas en la fila 1(excel)
+        if ($row > 1) { // comenzamos desde 1 para omitir el titulo de las comulnas ubicadas en la fila 1(excel)
             return true;
         }
         return false;
@@ -49,7 +51,7 @@ $inputFileType = \PhpOffice\PhpSpreadsheet\IOFactory::identify($inputFileName);
 $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
 
 // inicializar la Clase leer excel
-$reader->setReadFilter( new MyReadFilter() );
+$reader->setReadFilter(new MyReadFilter());
 
 /**  Cargar $inputFileName al objeto $Spreadsheet **/
 $spreadsheet = $reader->load($inputFileName);
@@ -59,25 +61,21 @@ $array_reg = $spreadsheet->getActiveSheet()->toArray();
 if (is_array($array_reg)) {
     foreach ($array_reg as $row) {
 
-        if(!is_null($row[0])){
-            $new_array['fechames'] = $row[0];
+        if (!is_null($row[0])) {
+            $fecha = new DateTime($row[0]);
+            $fecha_d_m_y = $fecha->format('Y/m/d');
+            $new_array['fechames'] = $fecha_d_m_y;
             $new_array['unidadnegocio'] = $row[1];
             $new_array['topcliente'] = $row[2];
             $new_array['ciudad'] = $row[3];
             $new_array['m3prod'] = $row[4];
-
-
-
-            
-       /** variable final para guardar en la base de datos $new_array */
-       $new_arrayf[] = $new_array;
+            /** variable final para guardar en la base de datos $new_array */
+            $new_arrayf[] = $new_array;
         }
-       
-       
     }
 }
 
-if($php_result= $cls_importdata->insert_pptoprod($new_arrayf)){
+if ($php_result = $cls_importdata->insert_pptoprod($new_arrayf)) {
     $php_estado = true;
 }
 
