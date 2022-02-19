2f4cc768e8de6e13dd547f1a556129a58f662583
hrcorporation <hr.ramirezoli@gmail.com>
2022-02-19 10:31:53 -0500

    crear archivos para eliminar

--- modules/Productos/caracteristica_concreto/update/editar.php
+++ modules/Productos/caracteristica_concreto/update/editar.php
@@ -50,32 +50,43 @@ if (is_array($datos_producto)) {
             <div class="card-header">
                 <h3 class="card-title">Modificar resistencia del concreto</h3>
                 <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip"
+                        title="Collapse">
                         <i class="fas fa-minus"></i></button>
-                    <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
+                    <button type="button" class="btn btn-tool" data-card-widget="maximize"><i
+                            class="fas fa-expand"></i></button>
                 </div>
             </div>
             <div class="card-body">
                 <div id="contenido">
-                    <form method="POST" name="FormModificarCaracteristicasConcreto" id="FormModificarCaracteristicasConcreto">
+                    <form method="POST" name="FormModificarCaracteristicasConcreto"
+                        id="FormModificarCaracteristicasConcreto">
                         <input type="hidden" name="txt_id" id="txt_id" value="<?php echo $id_producto ?>">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label> Codigo resistencia Concreto </label>
-                                    <input type="text" class="form-control" id="txt_CodCC" name="txt_CodCC" value="<?php echo $CodCC ?>">
+                                    <input type="text" class="form-control" id="txt_CodCC" name="txt_CodCC"
+                                        value="<?php echo $CodCC ?>">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label> Descripcion RC </label>
-                                    <input type="text" class="form-control" id="txt_DescripcionCC" name="txt_DescripcionCC" value="<?php echo $DescripcionCC ?>">
+                                    <input type="text" class="form-control" id="txt_DescripcionCC"
+                                        name="txt_DescripcionCC" value="<?php echo $DescripcionCC ?>">
                                 </div>
                             </div>
                         </div>
                         <div class="container">
                             <div class="row" style="text-align:center">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <button type="button" id="btn-eliminar" name="btn-eliminar"
+                                            class="btn btn-block btn-danger">Eliminar</button>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-block btn-success">Guardar</button>
                                     </div>
@@ -103,31 +114,52 @@ if (is_array($datos_producto)) {
 
 <?php include '../../../../layout/footer/footer4.php' ?>
 <script>
-    $(document).ready(function(e) {
-        $("#FormModificarCaracteristicasConcreto").on('submit', (function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: "php_editar.php",
-                type: "POST",
-                data: new FormData(this),
-                contentType: false,
-                cache: false,
-                processData: false,
-                success: function(data) {
-                    console.log(data);
-                    if (data.estado) {
-                        toastr.success('exitoso');
+$(document).ready(function(e) {
+    $("#FormModificarCaracteristicasConcreto").on('submit', (function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "php_editar.php",
+            type: "POST",
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(data) {
+                console.log(data);
+                if (data.estado) {
+                    toastr.success('exitoso');
 
-                    } else {
-                        toastr.warning(data.errores);
-                    }
-                },
-                error: function(respuesta) {
-                    alert(JSON.stringify(respuesta));
-                },
-            });
-        }));
+                } else {
+                    toastr.warning(data.errores);
+                }
+            },
+            error: function(respuesta) {
+                alert(JSON.stringify(respuesta));
+            },
+        });
+    }));
+
+    $("#btn-eliminar").click(function() {
+        var id = <?php echo $id_producto ?>;
+        $.ajax({
+            url: "php_eliminar.php",
+            type: "POST",
+            data: {
+                task: 1,
+                id: id,
+
+            },
+            success: function(response) {
+                toastr.success('Fue Eliminado Correctamente');
+                window.location = '../index.php'
+            },
+            error: function(respuesta) {
+                alert(JSON.stringify(respuesta));
+            },
+
+        });
     });
+});
 </script>
 </body>
 
