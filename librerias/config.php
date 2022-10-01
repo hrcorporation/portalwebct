e64d9bfccfbd50eb0f35ef93e36132e3c1d21fef
unknown <hr.ramirezoli@gmail.com>
2022-09-30 22:15:22 -0500

    visitas comerciales

--- librerias/config.php
+++ librerias/config.php
@@ -2,6 +2,6 @@
     define('DB_HOST','localhost');
     define('DB_USUARIO','root');
     define('DB_CONTRA','');
-    define('DB_NOMBRE','concr_bdportalconcretol');
+    define('DB_NOMBRE','portalconcretol');
     define('DB_CHARSET','utf8');
     define('DB_PORT','3306');
